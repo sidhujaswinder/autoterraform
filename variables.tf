35aef66acf80c7c6549b5d7e135076b7b56ca4d5
sidhujaswinder <44638773+sidhujaswinder@users.noreply.github.com>
2021-07-27 15:34:52 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIA355TIOJYEMWHW6NI"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "ap-south-1"
+  }
